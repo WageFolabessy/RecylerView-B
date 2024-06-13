da078b6347432be03dc55ee0ec6b2709a70fae5e
WageFolabessy <mbappepsg336@gmail.com>
2024-06-13 14:43:22 +0700

    selesai A

---
+++ app/src/main/java/com/garisas/myapplication/MahasiswaHolder.java
@@ -0,0 +1,23 @@
+package com.garisas.myapplication;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MahasiswaHolder extends RecyclerView.ViewHolder {
+    public ImageView _jkImageView;
+    public TextView _jkTextView, _jpTextView, _namaTextView, _nimTextView, _noTextView;
+    public MahasiswaHolder(View itemView) {
+        super(itemView);
+
+        _jkImageView = itemView.findViewById(R.id.jkImageView);
+        _jkTextView = itemView.findViewById(R.id.jkTextView);
+        _jpTextView = itemView.findViewById(R.id.jpTextView);
+        _namaTextView = itemView.findViewById(R.id.namaTextView);
+        _nimTextView = itemView.findViewById(R.id.nimTextView);
+        _noTextView = itemView.findViewById(R.id.noTextView);
+
+    }
+}
---
+++ app/src/main/java/com/garisas/myapplication/MainActivity.java
@@ -0,0 +1,81 @@
+package com.garisas.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import java.util.List;
+
+import cz.msebera.android.httpclient.Header;
+
+public class MainActivity extends AppCompatActivity {
+    private FloatingActionButton _addButton;
+    private RecyclerView _recyclerView;
+    private TextView _txtMahasiswaCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        _recyclerView = findViewById(R.id.recyclerView1);
+        _txtMahasiswaCount = findViewById(R.id.txtMahasiswaCount);
+
+        // initAddButton();
+        loadRecylerView();
+    }
+
+    private void loadRecylerView() {
+        AsyncHttpClient ahc = new AsyncHttpClient();
+        String url = "https://stmikpontianak.net/011100862/tampilMahasiswa.php";
+
+        ahc.get(url, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                Gson g = new Gson();
+                List<MahasiswaModel> mahasiswaModelList = g.fromJson(new String(responseBody), new TypeToken<List<MahasiswaModel>>(){}.getType());
+
+                RecyclerView.LayoutManager lm = new LinearLayoutManager(MainActivity.this);
+                _recyclerView.setLayoutManager(lm);
+
+                MahasiswaAdapter ma = new MahasiswaAdapter(mahasiswaModelList);
+                _recyclerView.setAdapter(ma);
+
+                String mahasiswaCount = "Total mahasiswa : " +ma.getItemCount();
+                _txtMahasiswaCount.setText(mahasiswaCount);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+//    private void initAddButton() {
+//        _addButton = findViewById(R.id.);
+//        _addButton.setOnClickListener(new View.OnClickListener(){
+//
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(getApplicationContext(), AddMahasiswaActivity.class);
+//                startActivity(intent);
+//
+//                loadRecylerView();
+//            }
+//        });
+//    }
+}
